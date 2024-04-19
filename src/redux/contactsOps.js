c22c36c404f45bb6778e2a8c2617eb228a1c3759
Apaccer <142975619+Apaccer@users.noreply.github.com>
2024-04-20 01:08:15 +0300

    hw-7-bm2

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -10,7 +10,6 @@ export const fetchContacts = createAsyncThunk(
   async (_, thunkApi) => {
     try {
       const data = await requestContacts();
-      console.log(data);
       return data;
     } catch (error) {
       return thunkApi.rejectWithValue(error.message);
